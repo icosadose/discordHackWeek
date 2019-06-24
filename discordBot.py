99189d73be6c4f8fcfeb6dce63e0780925adbcdc
12-Volt <bracc004@gmail.com>
2019-06-24 21:05:52 +0100

    Add files via upload

---
+++ discordBot.py
@@ -0,0 +1,17 @@
+import discord
+import logging
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    print(message)
+
+client.run('discord bot token')
