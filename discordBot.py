0be4c8a3f2043dd0abde350dcab5cf6b1e265412
12-Volt <bracc004@gmail.com>
2019-06-24 22:23:27 +0100

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -1,17 +1,22 @@
 import discord
-import logging
+import json
+import random as r
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    print(message)
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+    
+    if message.content.startswith("$text"):
+        await message.channel.send("*text* **text** ***text*** __text__ ~~text~~ `text`")
 
 client.run('discord bot token')
