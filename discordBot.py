d2a733fc2eebfb55826ffd77c289399fb6145d38
12-Volt <bracc004@gmail.com>
2019-06-25 23:06:27 +0100

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -1,8 +1,9 @@
 import discord
-import googletrans
+from googletrans import Translator, LANGCODES, LANGUAGES
 
 client = discord.Client()
-
+translator = Translator()
+targetLang = "en"
 
 @client.event
 async def on_ready():
@@ -10,7 +11,27 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global targetLang
     print(message.author, message.channel, message.content)
+    messageRaw = str(message.content)
+    messageLang = translator.detect(messageRaw).lang
+    print(targetLang)
+    print(messageLang)
+
+    if message.content.startswith("$") == False:
+        if messageLang != targetLang:
+            output = translator.translate(messageRaw, targetLang)
+            await message.channel.send(output)
+
+    if message.content.startswith("$language"):
+        words = messageRaw.split()
+        if len(words[1]) < 2 or len(words[1]) > 2:
+            await message.channel.send("Invalid language! Check the list using `$help 2`")
+        else:
+            targetLang = words[1]
+            changeLang = "The language will be changed to " + targetLang
+            await message.channel.send(changeLang)
+
     if message.author == client.user:
         return
 
@@ -19,11 +40,21 @@ async def on_message(message):
 
     if message.content.startswith("$help"):
         await message.channel.send("""
-        `$help 1 - commands`\n
-        `$help 2 - languages`\n
+        *Help Menu*
+        `$help 1 - commands`
+        `$help 2 - languages`
+        """)
+    
+    if message.content.startswith("$help 1"):
+        await message.channel.send("""
+        *Current Commands For The Bot*
+        `$help`     - Displays the help menu
+        `$version`  - Displays the version of discord.py
+        `$language` - Allows a user to change the target language
+        `$test`     - Returns content of a message
         """)
 
-    if message.content.startswith("$language"):
-
+    if message.content.startswith("$test"):
+        pass
 
 client.run("discord bot token")
